e800b42ef6cd0b223ea42b3637b71d600edf621d
rplinzner <37122411+rplinzner@users.noreply.github.com>
2018-05-01 22:41:52 +0200

    RM Weather initial commit

---
+++ RM Weather/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace RM_Weather
+{
+    [Activity(Label = "RM_Weather", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
